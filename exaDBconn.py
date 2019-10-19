e11cec17e63613a34f9ba27eece03b9deb873a36
Lucas Whitmire <lucas.whitmire@gmail.com>
2019-10-19 22:46:13 +0200

    updated locator.py to perform the query based on USSTATES table

--- exaDBconn.py
+++ exaDBconn.py
@@ -2,4 +2,5 @@ import pyexasol as pe
 
 def genDBCursor():
     C=pe.connect(dsn='192.168.56.102:8563',user='sys',password='exasol')
+    C.execute("OPEN SCHEMA AOL_SCHEMA;")
     return C
--- locator.py
+++ locator.py
@@ -2,22 +2,56 @@ from exaDBconn import genDBCursor
 import json
 
 def wrapStringsinDict (s):
+    """ s: dict containing search strings
+    Returns a dict with the values wrapped in SQL wildcards '%' """
     result={}
     for i in s:
         result[i]='%'+s[i]+'%'
     return result
 
-curs = genDBCursor()
-curs.execute("OPEN SCHEMA AOL_SCHEMA;")
-locations = curs.execute("SELECT ZIPCODE, CITY, STATE_NAME, STATE_ABBR\
-                        FROM AOL_SCHEMA.US_GEO")
-cityDict ={}
-for row in locations:
-    cityDict[row[0]] = (row[1],row[2],row[3])
+def getCityInfo():
+    curs = genDBCursor()
+    locations = curs.execute("SELECT ZIPCODE, CITY, STATE_NAME, STATE_ABBR\
+                            FROM AOL_SCHEMA.US_GEO")
+    cityDict ={}
+    for row in locations:
+        cityDict[row[0]] = (row[1],row[2],row[3])
+    return cityDict
 
-resultCount = 0    
-for i in cityDict: 
-    searchTerms = {'term0':cityDict[i][0],'term1':cityDict[i][0].lower(),'term2':cityDict[i][0].title(), 'term3':" "+cityDict[i][0],'term4':" "+cityDict[i][0]+" "}
+def getStateInfo():
+    """Returns a list of tuples with (STATE_ABBR, STATE_NAME) pairs"""
+    curs = genDBCursor()
+    states = curs.execute("SELECT STATE_ABBR, STATE_NAME \
+                            FROM USSTATES;")
+    stateKeywords =[]
+    for row in states:
+        stateKeywords.append((row[0],row[1]))
+    return stateKeywords
+
+def buildSearchDict(x):
+    NotImplemented
+    
+
+curs = genDBCursor()
+stateKeywords = getStateInfo()     
+resultCount = 0
+dataDict = {}    
+for i in range(len(stateKeywords)): 
+    
+    # Check for blanks in stateKeywords
+    if not bool(stateKeywords[i][0]) or not bool(stateKeywords[i][1]):
+        continue
+
+    # Build up search terms dict for use in query
+    searchTerms = {'term0':stateKeywords[i][0]+' %',\
+                'term1':"% "+stateKeywords[i][0].lower()+' %',\
+                'term2':"% "+stateKeywords[i][0]+' %', \
+                'term3':stateKeywords[i][0].lower()+' %',\
+                'term4':"% "+stateKeywords[i][0], \
+                'term5':'%'+stateKeywords[i][1]+'%',\
+                'term6':'%'+stateKeywords[i][1].lower()+'%',\
+                'term7':'%'+stateKeywords[i][1].title()+'%'}
+
     query = "SELECT ANONIDDIM.ID, irsQuery.query FROM\
             (SELECT QUERYDIM.QUERY as query, FACTS.ANONID as anonid\
             FROM FACTS\
@@ -37,17 +71,18 @@ for i in cityDict:
             query =query+" OR irsQuery.query LIKE {term"+str(j)+"};"
         else:    
             query = query+" OR irsQuery.query LIKE {term"+str(j)+"}"
-         
+
+    # Provide some status output     
     print(query)
-    print('searching: '+ cityDict[i][0])
+    print('searching: '+ str(stateKeywords[i]))
+    # Run Query
+    results = curs.execute(query,searchTerms)
 
-    results = curs.execute(query,wrapStringsinDict(searchTerms))
-
-    dataDict = {}
+    # Build dict of query results
     for res in results:
-        dataDict[resultCount]= {'user':str(res[0]),'query':str(res[1]),'city':cityDict[i][0],'state':cityDict[i][1]}
+        dataDict[resultCount]= {'user':str(res[0]),'query':str(res[1]),'abbr':stateKeywords[i][0],'state':stateKeywords[i][1]}
         resultCount+=1
         
-
+# Write dataDict to a json file
 with open('./datasrc/locResults.json', 'w') as fp:
     json.dump(dataDict, fp)
